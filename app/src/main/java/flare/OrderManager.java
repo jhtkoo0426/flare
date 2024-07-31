21283b3dbf6dc15e881ddaaf64649a9cf088ea45
Justin Koo <jhtkoo@gmail.com>
2024-07-31 17:26:29 +0100

    OrderManager: Added method to log orders to file

--- app/src/main/java/flare/OrderManager.java
+++ app/src/main/java/flare/OrderManager.java
@@ -1,6 +1,9 @@
 package flare;
 
 
+import java.io.*;
+
+
 public class OrderManager {
 
     private SharedDataStorage storage;
@@ -21,8 +24,23 @@ public class OrderManager {
         return storage.getCurrentOrderId();
     }
 
+    /**
+     * Registers order data by printing it to the console and writing it to a CSV log file.
+     *
+     * @param orderId  the unique identifier for the order
+     * @param symbol   the symbol of the security being ordered
+     * @param secType  the type of security (e.g., stock, bond)
+     * @param quantity the quantity of the security being ordered
+     */
     public void registerOrderData(int orderId, String symbol, String secType, double quantity) {
-        System.out.printf("Order ID %d registered for order - %s (%s) @ %.2f", orderId, symbol, secType, quantity);
+        System.out.printf("Order ID %d registered for order - %s (%s) @ %.2f\n", orderId, symbol, secType, quantity);
+        String line = String.format("%d,%s,%s,%f\n", orderId, symbol, secType, quantity);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/flare/logs/order_logs.csv"))) {
+            writer.write(line);
+        } catch (IOException e) {
+            System.err.println("[OrderManager] Error saving last order ID: " + e.getMessage());
+        }
     }
 
 }
